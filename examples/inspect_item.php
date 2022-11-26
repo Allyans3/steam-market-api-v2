cb0ae16ec0d7f8773e2cb2d48601c3842bc7e2ff
allyans3 <a.sylenok@gmail.com>
2022-11-27 00:33:14 +0100

    Examples

---
+++ examples/inspect_item.php
@@ -0,0 +1,11 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use SteamApi\SteamApi;
+
+
+$api = new SteamApi();
+
+$inspectLink = "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M3267882520477718672A19612828092D2642074259339382751";
+
+dd($api->detailed()->inspectItem($inspectLink));
